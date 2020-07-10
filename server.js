05b3e9c2c50d86e4315b6f1ff6adf97b8ea1b28f
Evan <evanmgreen063@gmail.com>
2020-07-10 18:21:00 -0400

    user post verification added

--- server.js
+++ server.js
@@ -2,9 +2,13 @@ const express = require('express');
 const connectDB = require('./config/db')
 
 const app = express();
+app.use(express.json());
 
 //Connect Database
 connectDB();
+
+//Init middleware
+app.use(express.json({ extend: false }));
 
 app.get('/', (req, res) => res.send('API RUNNING'));
 
@@ -13,6 +17,8 @@ app.use('/api/users', require('./routes/api/users'))
 app.use('/api/auth', require('./routes/api/auth'))
 app.use('/api/profile', require('./routes/api/profile'))
 app.use('/api/post', require('./routes/api/post'))
+
+
 
 const PORT = process.env.PORT || 3000;
 
